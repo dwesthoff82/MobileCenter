b16e58d017436456d492c69d9d38d76d20ab5b0a
dawestho <d.a.westhoff@gmail.,com>
2016-11-19 09:36:21 -0800

    Added changes for mobile center

--- app/src/main/java/com/example/dawestho/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dawestho/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.dawestho.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MobileCenter.start(getApplication(), "19ad565c-cfeb-4642-ae45-d8a816e51fb3",
+                Analytics.class, Crashes.class);
 
 
 
-        
+        //This is my first time using sonoma.... 
+
+
     }
 }
